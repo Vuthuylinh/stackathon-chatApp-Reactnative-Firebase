0fde025245fb055dbf9973d3964bc59e274c7d98
Linh Vu <thuylinh2490@gmail.com>
2021-01-24 21:29:51 -0500

    refactor: split App.js into navigation folder

--- App.js
+++ App.js
@@ -1,54 +1,8 @@
-
-import React,{useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from "@react-navigation/native"
-import { createStackNavigator } from "@react-navigation/stack"
-import AsyncStorage from '@react-native-community/async-storage'
-
-import OnboardingScreen from './Screens/OnboardingScreen'
-import LoginScreen from './Screens/LoginScreen'
-
-const AppStack = createStackNavigator()
+import React from 'react';
+import Providers from './navigation';
 
 const App = () => {
-  const [isFirstLaunch,setIsFirstLaunch] = useState(null);
-
-  useEffect(() => {
-    AsyncStorage.getItem('alreadyLaunched').then(value => {
-      if(value === null){
-        AsyncStorage.setItem('alreadyLaunched','true')
-        setIsFirstLaunch(true)
-      }else{
-        setIsFirstLaunch(false)
-      }
-    })
-  },[])
-
-  if(isFirstLaunch === null){
-    return null
-  }else if(isFirstLaunch === true){
-    return(
-      <NavigationContainer>
-        <AppStack.Navigator headerMode="none">
-          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-          <AppStack.Screen name="Login" component={LoginScreen} />
-        </AppStack.Navigator>
-      </NavigationContainer>
-    )
-  }else{
-    return
-      <LoginScreen/>
-  }
-
-
+  return <Providers />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-export default App
+export default App;
---
+++ navigation/AuthStack.js
@@ -0,0 +1,60 @@
+import React,{useState, useEffect} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack"
+
+import AsyncStorage from '@react-native-community/async-storage'
+
+import OnboardingScreen from '../screens/OnboardingScreen'
+import LoginScreen from '../screens/LoginScreen'
+
+const Stack = createStackNavigator()
+
+const AuthStack = () => {
+  const [isFirstLaunch,setIsFirstLaunch] = useState(null);
+  let routeName;
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value == null){
+        AsyncStorage.setItem('alreadyLaunched','true')
+        setIsFirstLaunch(true)
+      }else{
+        setIsFirstLaunch(false)
+      }
+    })
+  },[])
+
+  if(isFirstLaunch === null){
+    return <Text> Loading...</Text>
+  }else if(isFirstLaunch === true){
+    return(
+        <Stack.Navigator initialRouteName={routeName}>
+          <Stack.Screen
+          name="Onboarding"
+          component={OnboardingScreen}
+          options={{header:()=>null}}
+          />
+          <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{header:()=>null}}
+          />
+
+        </Stack.Navigator>
+    )
+  }else{
+    return
+      <LoginScreen/>
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default AuthStack
