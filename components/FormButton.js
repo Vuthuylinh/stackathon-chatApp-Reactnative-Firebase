42d897cd79220a3265d2437f9d5bb5f4cbbff9bb
Linh Vu <thuylinh2490@gmail.com>
2021-01-24 22:35:33 -0500

    feat(component): create SocialButton

--- components/FormButton.js
+++ components/FormButton.js
@@ -1,9 +1,22 @@
-import { StyleSheet, Text,  TouchableOpacity } from 'react-native'
-import{windowHeight,windowWidth} from '../utils/Dimentions'
-const FormButton = ({buttonTitle, ...rest}) => {
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { windowHeight, windowWidth } from '../utils/Dimentions'
+const FormButton = ({
+  buttonTitle,
+  btnType,
+  color,
+  backgroundColor,
+  ...rest }) => {
+  let bgColor = backgroundColor;
   return (
-    <TouchableOpacity style={styles.buttonContainer} {...rest}>
-      <Text style={styles.buttonText}></Text>
+    <TouchableOpacity
+    style={[styles.buttonContainer, {backgroundColor: bgColor}]}{...rest}>
+      <View style={styles.iconWrapper}>
+         <FontAwsome style={styles.icon} name={btnType} size={22} color={color}/>
+      </View>
+      <View style={styles.btnTxtWrapper}>
+         <Text style={[styles.buttonText, {color:color}]}>{buttonTitle}</Text>
+      </View>
+
     </TouchableOpacity>
 
   )
@@ -12,21 +25,31 @@ const FormButton = ({buttonTitle, ...rest}) => {
 export default FormButton
 
 const styles = StyleSheet.create({
-  buttonContainer ={
-    marginTop:10,
+  buttonContainer: {
+    marginTop: 10,
     width: '100%',
-    height: windowHeight/15,
-    backgroundColor:'#2e64e5',
+    height: windowHeight / 15,
     padding: 10,
-    alignItems: 'center',
-    justifyContent:'center',
-    borderRadius:3
+    flexDirection: 'row',
+    borderRadius: 3,
   },
-  buttonText:{
+  iconWrapper: {
+    width: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    fontWeight: 'bold',
+  },
+  btnTxtWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
     fontSize: 18,
-    fontWeight:'bold',
-    color:'#ffffff',
-    fontFamily:'Lato-Regular'
-   }
-})
+    fontWeight: 'bold',
+    fontFamily: 'Lato-Regular',
+  },
+});
 
