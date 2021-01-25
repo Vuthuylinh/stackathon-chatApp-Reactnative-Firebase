05ca14e0c270eff36d162fae55ff45307d6336f4
Linh Vu <thuylinh2490@gmail.com>
2021-01-24 20:41:28 -0500

    feat(App.js): check isFirstLaunch to decide show OnboardingScreen or LoginScreen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
+import AsyncStorage from '@react-native-community/async-storage'
 
 import OnboardingScreen from './Screens/OnboardingScreen'
 import LoginScreen from './Screens/LoginScreen'
@@ -10,14 +11,36 @@ import LoginScreen from './Screens/LoginScreen'
 const AppStack = createStackNavigator()
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator headerMode="none">
-        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-        <AppStack.Screen name="Login" component={LoginScreen} />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+  const [isFirstLaunch,setIsFirstLaunch] = useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value === null){
+        AsyncStorage.setItem('alreadyLaunched','true')
+        setIsFirstLaunch(true)
+      }else{
+        setIsFirstLaunch(false)
+      }
+    })
+  },[])
+
+  if(isFirstLaunch === null){
+    return null
+  }else if(isFirstLaunch === true){
+    return(
+      <NavigationContainer>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+          <AppStack.Screen name="Login" component={LoginScreen} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    )
+  }else{
+    return
+      <LoginScreen/>
+  }
+
+
 }
 
 const styles = StyleSheet.create({
